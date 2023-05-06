97dfd8f7e87bf4d18c713dde000df29bf4edf82a
Frank Orac <110904168+FrankOrac@users.noreply.github.com>
2023-05-06 18:05:16 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+// console.log("hELLO WORLD");
+
+let age = 20;
+age= 21;
+console.log(age);
+let name = "Bash";
+let royal= null ;
+
+// console.log(age);
+console.log((23*52)%7);
+
+
+
+
+// alert("this is coding class");
+
+// let X =prompt("What is your FIRST name");
+// let x11 =prompt("What is your LAST name");
+// let X2 =prompt("hOW old are you");
+// let X3 =prompt("What class are you");
+// let X4 =prompt("What is your best color");
+
+
+// document.getElementById('h1').onclick {
+
+// }
+
+
+// const x11 = 18;
+// x11 =19;
+// console.log(x11);
